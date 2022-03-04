2944b4132cb2a6dd27ea98e496f63aaa2d6341b2
alireza ketabi <alirezaketabi.h@gmail.com>
2022-03-04 21:36:29 +0330

    write get post delete patch function in Router

--- src/Router.php
+++ src/Router.php
@@ -7,8 +7,28 @@ class Router
 {
     private array $routs;
 
+    public function get(string $path, callable $callable)
+    {
+        $this->SetRoute($path, Method::GET, $callable);
+    }
+
     private function SetRoute(string $path, string $method, callable $callable)
     {
         $this->routs[$method][] = new Route($path, $callable);
+    }
+
+    public function post(string $path, callable $callable)
+    {
+        $this->SetRoute($path, Method::POST, $callable);
+    }
+
+    public function delete(string $path, callable $callable)
+    {
+        $this->SetRoute($path, Method::DELETE, $callable);
+    }
+
+    public function patch(string $path, callable $callable)
+    {
+        $this->SetRoute($path, Method::PATH, $callable);
     }
 }
