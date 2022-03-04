c5abbccfe9bb4401f75b703f562d4cc03a63f4d4
alireza ketabi <alirezaketabi.h@gmail.com>
2022-03-04 21:30:10 +0330

    write type of the methods

---
+++ src/Method.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Alireza\Router;
+
+class Method
+{
+    public const GET = "GET";
+    public const POST = "POST";
+    public const DELETE = "DELETE";
+    public const PATH = "PATCH";
+    public const PUT = "PUT";
+}
