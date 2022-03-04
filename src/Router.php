efebdd04925d907b3915a1f5ba453367054bcb63
alireza ketabi <alirezaketabi.h@gmail.com>
2022-03-04 21:33:46 +0330

    write function SetRoute in Router

--- src/Router.php
+++ src/Router.php
@@ -5,5 +5,10 @@ namespace Alireza\Router;
 
 class Router
 {
+    private array $routs;
 
+    private function SetRoute(string $path, string $method, callable $callable)
+    {
+        $this->routs[$method][] = new Route($path, $callable);
+    }
 }
