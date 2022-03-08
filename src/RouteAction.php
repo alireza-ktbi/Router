77d8d6bb34e6fd53a5cd9be0663e68d8c39395b7
alireza ketabi <alirezaketabi.h@gmail.com>
2022-03-08 18:48:57 +0330

    some changes in execute function RouteAction

--- src/RouteAction.php
+++ src/RouteAction.php
@@ -11,13 +11,18 @@ class RouteAction
         $this->action = $action;
     }
 
-    public function execute(array $args = null)
+    public function execute(array $args = [])
     {
         if (is_callable($this->action)) {
             ($this->action)($args);
         } elseif (is_array($this->action)) {
             if (count($this->action) == 2) {
-                call_user_func_array($this->action, $args);
+                if (class_exists($this->action[0]) && method_exists($this->action[0], $this->action[1])) {
+                    if (is_string($this->action[0])) {
+                        $this->action[0] = new $this->action[0];
+                    }
+                    $this->action[0]->{$this->action[1]}($args);
+                }
             }
         }
     }
